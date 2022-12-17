fa3c23577c8da5aa33d8d0554f050b5360a11663
Rajendra-Anargya <120623144+Rajendra-Anargya@users.noreply.github.com>
2022-12-17 19:53:17 +0700

    Add files via upload

---
+++ admin/page_home.php
@@ -0,0 +1,19 @@
+<div class="card im-box">
+    <h5 class="card-header">Selamat Datang</h5>
+    <div class="card-body">
+        <h5 class="card-title">CRUD PHP dengan Tema Bela Negara</h5>
+        <p class="card-text">Kita tidak boleh berhenti berkreasi, berinovasi, dan berprestasi. Kita harus buktikan
+            ketangguhan kita. Kita harus menangkan masa depan kita dan kita wujudkan cita-cita para Pendiri Bangsa
+            dengan semangat bela negara.<br><br>Anggota:<br>1.Achmad Fasya Ardiza Setiawan_77222318_77.9E2.07
+            <br>2.Aditya Dwi Cahyo_77222321_77.9E2.07 <br>3.Ferry Andriansyah_77222387_77.9E2.07 <br>4.Moh
+            Holik_77222313_77.9E2.07 <br>5.Rajendra Anargya_77222312_77.9E2.07 <br>6.Zacky Firdaus_77222317_77.9E2.07
+        </p>
+        <p>
+            <br><b>Edited by Rajendra Anargya</b>
+        </p>
+        <p>
+            <b>Asisted by Gilang M.R Iwa Kusumah</b>
+        </p>
+        <!--<a href="page_about.php" class="btn btn-primary">Learn More</a>-->
+    </div>
+</div>
